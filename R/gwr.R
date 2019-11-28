1a3e950b6f8873d843c73e8577cb59da37bfd3ad
graciellehigino <graciellehigino@hotmail.com>
2019-11-28 16:30:53 -0500

    starting gwr

--- R/gwr.R
+++ R/gwr.R
@@ -1,12 +1,22 @@
 library(spgwr)
 library(parallel)
 
-gwr.env<-env %>% group_by(cells) %>% summarise_all(list(mean))
+gwr.env<-env %>% group_by(cells) %>% summarise_all(mean, na.rm = TRUE)
 gwr.df<-left_join(gwr.df,gwr.env, by = "cells")
 gwr.df<-left_join(gwr.df,sr, by = "cells")
 gwr.df<-as.data.frame(gwr.df)
+gwr.df<-na.exclude(gwr.df)
 save(gwr.df, file="output/results/df_by_cell.Rdata")
 
-g <- gwr.sel(gwr.df$slope.sr ~ gwr.df$topography, gwr.df, coords = cbind(gwr.df$longitude, gwr.df$latitude), gweight = gwr.Gauss(), method = "cv", adapt = TRUE)
+bw <- gwr.sel(slope.sr ~ temperature, data=gwr.df, coords = cbind(gwr.df$longitude, gwr.df$latitude))
 c <- makeCluster(20, type="FORK")
-g.model <- gwr(gwr.df$slope.sr ~ gwr.df$topography, gwr.df, coords = cbind(gwr.df$longitude, gwr.df$latitude), longlat = TRUE, bandwidth = g, gweight = gwr.Gauss(), method = "cv", cl = c)
+clusterEvalQ(c, library(spgwr))
+xy<-cbind(gwr.df$longitude, gwr.df$latitude)
+g.model <- gwr(slope.sr ~ temperature, data=gwr.df, coords = xy, bandwidth = bw, cl = c)
+
+bw<-vector(length=4)
+g.model<-vector(length=4)
+for (i in 1:4){
+    bw[i]<-gwr.sel(slope.sr ~ gwr.df[,i+5], data=gwr.df, coords = cbind(gwr.df$longitude, gwr.df$latitude))
+    g.model[i] <- gwr(slope.sr ~ gwr.df[,i+5], data=gwr.df, coords = cbind(gwr.df$longitude, gwr.df$latitude), bandwidth = bw[i], cl = c)
+}
