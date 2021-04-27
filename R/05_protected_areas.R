d36e97acc530b11fa56440b922606abdd9f30031
graciellehigino <graciellehigino@hotmail.com>
2021-04-27 17:04:43 -0400

    add protected areas shp

---
+++ R/05_protected_areas.R
@@ -0,0 +1,15 @@
+pa1 <- readOGR("data/raw/pa-south_america/WDPA_WDOECM_Apr2021_Public_SA_shp/WDPA_WDOECM_Apr2021_Public_SA_shp_0/WDPA_WDOECM_Apr2021_Public_SA_shp-polygons.shp")
+pa1 <- pa1[ma,]
+pa1 <- pa1[pa1@data$REP_M_AREA==0,]
+
+pa2 <- readOGR("data/raw/pa-south_america/WDPA_WDOECM_Apr2021_Public_SA_shp/WDPA_WDOECM_Apr2021_Public_SA_shp_1/WDPA_WDOECM_Apr2021_Public_SA_shp-polygons.shp")
+pa2 <- pa2[ma,]
+pa2 <- pa2[pa2@data$REP_M_AREA==0,]
+
+pa3 <- readOGR("data/raw/pa-south_america/WDPA_WDOECM_Apr2021_Public_SA_shp/WDPA_WDOECM_Apr2021_Public_SA_shp_2/WDPA_WDOECM_Apr2021_Public_SA_shp-polygons.shp")
+pa3 <- pa3[ma,]
+pa3 <- pa3[pa3@data$REP_M_AREA==0,]
+
+pa <- raster::bind(pa1, pa2, pa3)
+rm(pa1, pa2, pa3)
+
