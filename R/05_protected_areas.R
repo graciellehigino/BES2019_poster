58c06acd5466c65f63a452b0f521b34f4160ec27
graciellehigino <graciellehigino@hotmail.com>
2021-05-11 18:21:44 -0400

    protected area - distances and raster

--- R/05_protected_areas.R
+++ R/05_protected_areas.R
@@ -13,7 +13,17 @@ pa3 <- pa3[pa3@data$REP_M_AREA==0,]
 pa <- raster::bind(pa1, pa2, pa3)
 rm(pa1, pa2, pa3)
 
-r <- raster(ext=extent(ma), resolution=0.009)
+r <- raster(ext=extent(ma), resolution=0.09)
 pa_raster <- rasterize(pa, r)
 
 # Distance from PA------
+pa_coords <- coordinates(pa_raster)
+dist_pa <- geodist::geodist(pa_coords, gwr.df[,c(2,3)], measure="geodesic")
+d <-as.numeric()
+for(i in 1:nrow(gwr.df)){
+    d[i] <- min(geodist::geodist(pa_coords, gwr.df[i,c(2,3)], measure="geodesic"))
+}
+
+# Distances + GWR.DF -----
+gwr.df <- cbind(gwr.df, d)
+save(gwr.df, file="data/clean/gwr_df.Rdata")
